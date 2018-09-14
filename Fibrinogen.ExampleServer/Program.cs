0e40696fe5c945d3c6aa54bdef391d0319528e7d
phillyai <sonagi.philly@gmail.com>
2018-09-14 11:31:26 +0900

    예제 서버 프로젝트 추가

---
+++ Fibrinogen.ExampleServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace Fibrinogen.ExampleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.Register(Callback);
+            Server.Start();
+            Console.Write("Server is running! press ENTER key to stop");
+            Console.Read();
+            Server.Stop();
+        }
+
+        static string Callback(HttpListenerRequest request)
+        {
+            return $@"
+<body>
+    <h2>Segments: </h2>
+    {string.Join(' ', request.Url.Segments)}
+    <br>
+    <h2>Query: </h2>
+    {request.Url.Query}
+</body>";
+        }
+    }
+}
