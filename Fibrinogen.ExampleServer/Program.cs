6c959aa15981e92617d26ddc465b081ce1b64920
phillyai <sonagi.philly@gmail.com>
2018-09-14 12:00:07 +0900

    예제 마크다운 서버 구현

--- Fibrinogen.ExampleServer/Program.cs
+++ Fibrinogen.ExampleServer/Program.cs
@@ -7,23 +7,11 @@ namespace Fibrinogen.ExampleServer
     {
         static void Main(string[] args)
         {
-            Server.Register(Callback);
-            Server.Start();
-            Console.Write("Server is running! press ENTER key to stop");
+            MarkdownServer.BaseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            MarkdownServer.Start();
+            Console.Write("MarkdownServer is running! press ENTER key to stop");
             Console.Read();
-            Server.Stop();
-        }
-
-        static string Callback(HttpListenerRequest request)
-        {
-            return $@"
-<body>
-    <h2>Segments: </h2>
-    {string.Join(' ', request.Url.Segments)}
-    <br>
-    <h2>Query: </h2>
-    {request.Url.Query}
-</body>";
+            MarkdownServer.Stop();
         }
     }
 }
